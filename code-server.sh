d4fc51ecb2c822e2247b4a3cb5730d148c25aac1
Ammer Ashraf Emon <aae.emon96@gmail.com>
2022-07-03 18:06:38 +0600

    added new

---
+++ code-server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Time Zone: " timezone
+read -p "Password: " password
+read -p "Sudo Password: " sudo_password
+
+echo "Waiting to Pull the latest image..."
+
+docker run -d \
+  --name=code-server \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e "TZ=${timezone}" \
+  -e "PASSWORD=${password}" \
+  -e "SUDO_PASSWORD=${sudo_password}" \
+  -e HASHED_PASSWORD= `#optional` \
+  -e SUDO_PASSWORD_HASH= `#optional` \
+  -e PROXY_DOMAIN=code-server.my.domain `#optional` \
+  -e DEFAULT_WORKSPACE=/config/workspace `#optional` \
+  -p 8443:8443 \
+  -v /portainer/Files/AppData/Config/code-server:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/code-server:latest || error "Failed to run Code-Server docker image!"
---
+++ duckdns.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read -p "Time Zone: " timezone
+read -p "Sub Domains [Use comma(,) to separate]: " subdomains
+read -p "Token: " token
+
+echo "Waiting to Pull the latest image..."
+
+sudo docker pull lscr.io/linuxserver/duckdns:latest || error "Failed to pull the latest DuckDNS docker image!"
+
+docker run -d \
+  --name=duckdns \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e "TZ=${timezone}" \
+  -e "SUBDOMAINS=${subdomains}" \
+  -e "TOKEN=${token}" \
+  -e LOG_FILE=false \
+  -v /portainer/Files/AppData/Config/duckdns:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/duckdns:latest || error "Failed to run DuckDNS docker image!"
