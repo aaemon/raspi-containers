705db733d634b9403dcad8f46b5f6517820c9185
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-07-04 11:11:33 +0000

    updated

---
+++ duplicati.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Time Zone: " tz
+echo "Waiting to pull the latest image..."
+
+sudo docker pull lscr.io/linuxserver/duplicati:latest || error "Failed to pull Duplicati docker image!"
+
+docker run -d \
+    --name=duplicati \
+    -e PUID=1000 \
+    -e PGID=1000 \
+    -e TZ=${tz} \
+    -e CLI_ARGS= `#optional` \
+    -p 8200:8200 \
+    -v /portainer/Files/AppData/Config/duplicati/config:/config \
+    -v /portainer/Files/AppData/Config/duplicati/backups:/backups \
+    -v /portainer/Files/AppData/Config/duplicati/source:/source \
+    --restart unless-stopped \
+    lscr.io/linuxserver/duplicati:latest || error "Failed to run Duplicati docker image!"
