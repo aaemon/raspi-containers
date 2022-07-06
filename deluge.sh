d1b6f898d5e936acc2ee54aa816fb1eb3ff761c9
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-07-06 03:45:08 +0000

    updated

---
+++ deluge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "Time Zone: " tz
+echo "Waiting to pull the latest image..."
+
+sudo docker pull lscr.io/linuxserver/deluge:latest || error "Failed to pull Deluge docker image!"
+
+sudo docker run -d \
+  --name=deluge \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=${tz} \
+  -e DELUGE_LOGLEVEL=error `#optional` \
+  -p 8112:8112 \
+  -p 6881:6881 \
+  -p 6881:6881/udp \
+  -v /portainer/Files/AppData/Config/deluge/config:/config \
+  -v /media/nfs/downloads:/downloads \
+  --restart unless-stopped \
+  lscr.io/linuxserver/deluge:latest || error "Failed to run Deluge docker container!"
