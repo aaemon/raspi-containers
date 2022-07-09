e5c67b782eadc21a9293cf2100b5e15cb183a6ed
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-07-09 04:18:49 +0000

    updated

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-read -s -p "Enter MySQL Root Password: " pswd
+read -s -p "Enter MySQL Root Password: " password
 echo ""
 echo "Waiting to pull the latest image..."
 
-sudo docker pull mysql:8.0.29-oracle || error "Failed to pull MySQL docker image!"
+sudo docker pull mysql:latest || error "Failed to pull MySQL docker image!"
 
 sudo docker run -d \
   --name mysql \
   -p 3306:3306 \
   -v /var/lib/mysql:/var/lib/mysql \
-  -e MYSQL_ROOT_PASSWORD=${pswd} \
+  -e MYSQL_ROOT_PASSWORD=${password} \
   --restart unless-stopped \
-  mysql:8.0.29-oracle || error "Failed to run MySQL docker container!"
+  mysql:latest || error "Failed to run MySQL docker container!"
