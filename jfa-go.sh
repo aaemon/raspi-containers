6ea332f1c90522bfcb1ee35252efd3dec4db5c4d
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-07-11 05:10:28 +0000

    added

---
+++ jfa-go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Waiting to pull the latest image..."
+
+sudo docker pull hrfee/jfa-go:latest || error "Failed to pull Jfa-Go docker image!"
+
+sudo docker run -d \
+    --name "jfa-go" \ 
+    -p 8056:8056 \
+    # -p 8057:8057 if using tls
+    -v /portainer/Files/AppData/Config/jfa-go:/data \
+    -v /portainer/Files/AppData/Config/jellyfin:/jf \
+    -v /etc/localtime:/etc/localtime:ro \
+    --restart unless-stopped \
+    hrfee/jfa-go:latest || error "Failed to run Jfa-Go docker container!"
