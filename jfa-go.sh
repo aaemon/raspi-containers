1e5ee74484fa027f88a3472b0f614a3da61ecce6
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-07-11 06:06:34 +0000

    updated

--- jfa-go.sh
+++ jfa-go.sh
@@ -7,7 +7,7 @@ sudo docker pull hrfee/jfa-go:latest || error "Failed to pull Jfa-Go docker imag
 sudo docker run -d \
     --name "jfa-go" \
     -p 8056:8056 \
-    # -p 8057:8057 if using tls
+    # -p 8057:8057 if using tls \
     -v /portainer/Files/AppData/Config/jfa-go:/data \
     -v /portainer/Files/AppData/Config/jellyfin:/jf \
     -v /etc/localtime:/etc/localtime:ro \
