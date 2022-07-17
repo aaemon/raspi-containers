b75554cf519a8187a68132fa45f1695826c29b5f
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-07-17 13:11:58 +0000

    updated

---
+++ wikijs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Time Zone: " tz
+echo "Waiting to pull the latest image..."
+
+sudo docker pull lscr.io/linuxserver/wikijs:latest || error "Failed to pull WikiJS docker image!"
+
+sudo docker run -d \
+  --name=wikijs \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=${tz} \
+  -p 3008:3000 \
+  -v /portainer/Files/AppData/Config/wikijs/config:/config \
+  -v /portainer/Files/AppData/Config/wikijs/data:/data \
+  --restart unless-stopped \
+  lscr.io/linuxserver/wikijs:latest || error "Failed to run WikiJS docker image!"
