ac6f2ff804f4dbd46056d71acf1f448c2962566e
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2022-08-30 02:25:18 +0000

    updated

--- wireguard.sh
+++ wireguard.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-read -p "Time Zone: " tz
+read -p "Password: " password
 echo "Waiting to pull the latest image..."
 
-sudo docker pull linuxserver/wireguard:latest || error "Failed to pull Wireguard docker image!"
+sudo docker pull weejewel/wg-easy:latest || error "Failed to pull Wireguard docker image!"
 
 sudo docker run -d \
   --name=wireguard \
+  -e WG_HOST=192.168.31.48 \
+  -e PASSWORD=${password} \
+  -v /portainer/Files/AppData/Config/WireGuard:/etc/wireguard \
+  -p 51820:51820/udp \
+  -p 51821:51821/tcp \
   --cap-add=NET_ADMIN \
   --cap-add=SYS_MODULE \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=${tz} \
-  -e SERVERURL=wireguard.domain.com `#optional` \
-  -e SERVERPORT=51820 `#optional` \
-  -e PEERS=1 `#optional` \
-  -e PEERDNS=auto `#optional` \
-  -e INTERNAL_SUBNET=10.13.13.0 `#optional` \
-  -e ALLOWEDIPS=0.0.0.0/0 `#optional` \
-  -e LOG_CONFS=true `#optional` \
-  -p 51820:51820/udp \
-  -v /portainer/Files/AppData/Config/WireGuard:/config \
-  -v /lib/modules:/lib/modules \
   --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
+  --sysctl="net.ipv4.ip_forward=1" \
   --restart unless-stopped \
-  linuxserver/wireguard:latest || error "Failed to run Wireguard docker image!"
+  weejewel/wg-easy:latest || error "Failed to run Wireguard docker image!"
