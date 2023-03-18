b08e3a1108a7474cc456ef7a94bfd98475e4c77b
Ammer Ashraf Emon <30506675+aaemon@users.noreply.github.com>
2023-03-18 20:40:48 +0600

    Update synapse.sh
    need to fix

--- synapse.sh
+++ synapse.sh
@@ -15,3 +15,68 @@ sudo docker run -it --rm \
   -e GID=1000 \
   --network=cloudflare \
   matrixdotorg/synapse:latest generate || error "Failed to run Synapse docker container!"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  sudo docker run -it --rm \
+  -v /portainer/Files/AppData/Config/synapse:/data \
+  -e SYNAPSE_SERVER_NAME=matrix.dotscale.tech \
+  -e SYNAPSE_REPORT_STATS=yes \
+  -e UID=1000 \
+  -e GID=1000 \
+  matrixdotorg/synapse:latest generate
+
+# Add the following lines to homeserver.yml
+------------------------------------------------------
+database:
+    name: psycopg2
+    args:
+        user: synapse
+        password: somepassword
+        host: postgresql
+        database: synapse
+        cp_min: 5
+        cp_max: 10
+------------------------------------------------------
+# add postgres user
+
+createuser --pwprompt synapse_user
+
+createdb --encoding=UTF8 --locale=C --template=template0 --owner=synapse_user synapse
+
+======================================================
+
+docker run -d --name synapse \
+    -e UID=1000 \
+    -e GID=1000 \
+    -v /portainer/Files/AppData/Config/synapse:/data \
+    -p 8008:8008 \
+    matrixdotorg/synapse:latest
+
+
+
+# to generate account
+
+docker exec -it synapse register_new_matrix_user http://localhost:8008 -c /data/homeserver.yaml
